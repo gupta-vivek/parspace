f3fd43d5a7572eb36cafc53b3e965d4527918ef9
Vivek Gupta <vivek.gupta@razorthink.net>
2018-02-06 12:40:19 +0530

    Resize PKLot image to 56x56

--- pre-processing/resize.py
+++ pre-processing/resize.py
@@ -16,21 +16,18 @@ from PIL import Image
 import os
 
 
-car_path = "/home/vivek/Documents/car_detection/cars_train/"
-car_resize_path = "/home/vivek/Documents/car_detection/cars_train_resize/"
+PKLot_path = "../../PKLot"
+PKLot_resize_path = "../../PKLot_resize"
 
-not_cars_path = "/home/vivek/Documents/car_detection/not_cars/"
-not_cars_resize_path = "/home/vivek/Documents/car_detection/not_cars_resize/"
-
-for root, dirs, files in os.walk(not_cars_path):
+for root, dirs, files in os.walk(PKLot_path):
         for name in files:
             img = Image.open(os.path.join(root, name))
             img = img.resize((56, 56))
-            img.save(not_cars_resize_path + name, quality=100)
+            img.save(PKLot_resize_path + name, quality=100)
 
-for ind, file in enumerate(os.listdir(car_path)):
-    if ind % 100 == 0:
-        print(ind)
-    img = Image.open(car_path + file)
-    img = img.resize((56, 56))
-    img.save(car_resize_path + file, quality=100)
+# for ind, file in enumerate(os.listdir(car_path)):
+#     if ind % 100 == 0:
+#         print(ind)
+#     img = Image.open(car_path + file)
+#     img = img.resize((56, 56))
+#     img.save(car_resize_path + file, quality=100)
